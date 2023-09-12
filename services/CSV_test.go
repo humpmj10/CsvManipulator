43aac6d2d008b2268277fac07a818528c014a336
Michael Humphrey <michael.j.humphrey@gmail.com>
2023-09-12 16:06:20 -0700

    Added some tests and added test to github actions

---
+++ services/CSV_test.go
@@ -0,0 +1,53 @@
+package services
+
+import (
+	"encoding/csv"
+	"strings"
+	"testing"
+)
+
+func TestCsvService_SortCsv(t *testing.T) {
+	content := `name,age,email
+Bob,30,john@example.com
+Jane,25,jane@example.com
+Cindy,55,cindy@example.com`
+	r := csv.NewReader(strings.NewReader(content))
+	records, err := r.ReadAll()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	sortCSV(records, 0)
+
+	if records[1][0] != "Bob" {
+		t.Errorf("Expected Bob, got %s", records[1][0])
+	}
+	if records[2][0] != "Cindy" {
+		t.Errorf("Expected Cindy, got %s", records[2][0])
+	}
+	if records[3][0] != "Jane" {
+		t.Errorf("Expected Jane, got %s", records[3][0])
+	}
+
+	sortCSV(records, 1)
+
+	if records[1][1] != "25" {
+		t.Errorf("Expected 25, got %s", records[1][1])
+	}
+	if records[2][1] != "30" {
+		t.Errorf("Expected 30, got %s", records[2][1])
+	}
+	if records[3][1] != "55" {
+		t.Errorf("Expected 55, got %s", records[3][1])
+	}
+
+	sortCSV(records, 2)
+
+	if records[1][2] != "cindy@example.com" {
+		t.Errorf("Expected cindy@example.com, got %s", records[1][2])
+	}
+	if records[2][2] != "jane@example.com" {
+		t.Errorf("Expected jane@example.com, got %s", records[2][2])
+	}
+
+}
